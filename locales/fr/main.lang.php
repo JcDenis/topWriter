c9abe4063d39d660fe4811ea02d79f33a2149a19
Jean-Christian Denis <contact@jcdenis.fr>
2023-04-07 23:54:07 +0200

    fix plural

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -9,23 +9,27 @@
 # DOT NOT MODIFY THIS FILE !
 #
 
-l10n::$locales['Top writer: entries']                                      = 'Top rédacteur : billets';
-l10n::$locales['Top writer: comments']                                     = 'Top rédacteur : commentaires';
-l10n::$locales['Period:']                                                  = 'Période :';
-l10n::$locales['Limit:']                                                   = 'Limite :';
-l10n::$locales['List users who write more comments']                       = 'Liste les utilisateurs qui ont écrit le plus de commentaires';
-l10n::$locales['Top comments']                                             = 'Top commentaires';
-l10n::$locales['Exclude post writer from list']                            = 'Exclure de la liste les auteurs de billets';
-l10n::$locales['List users who write more posts']                          = 'Liste les utilisateurs qui ont écrit le plus de billets';
-l10n::$locales['Top entries']                                              = 'Top billets';
-l10n::$locales['Author posts']                                             = 'Billets de l\'auteur';
-l10n::$locales['Author link']                                              = 'Lien vers l\'auteur';
-l10n::$locales['no entries']                                               = 'aucun billet';
-l10n::$locales['one entry'][0]                                             = 'une publication';
-l10n::$locales['one entry'][1]                                             = '%s publications';
-l10n::$locales['no comments']                                              = 'aucun commentaire';
-l10n::$locales['one comment'][0]                                           = 'un commentaire';
-l10n::$locales['one comment'][1]                                           = '%s commentaires';
+l10n::$locales['Top writer: entries']                = 'Top rédacteur : billets';
+l10n::$locales['Top writer: comments']               = 'Top rédacteur : commentaires';
+l10n::$locales['Period:']                            = 'Période :';
+l10n::$locales['Limit:']                             = 'Limite :';
+l10n::$locales['List users who write more comments'] = 'Liste les utilisateurs qui ont écrit le plus de commentaires';
+l10n::$locales['Top comments']                       = 'Top commentaires';
+l10n::$locales['Exclude post writer from list']      = 'Exclure de la liste les auteurs de billets';
+l10n::$locales['List users who write more posts']    = 'Liste les utilisateurs qui ont écrit le plus de billets';
+l10n::$locales['Top entries']                        = 'Top billets';
+l10n::$locales['Author posts']                       = 'Billets de l\'auteur';
+l10n::$locales['Author link']                        = 'Lien vers l\'auteur';
+l10n::$locales['no entries']                         = 'aucun billet';
+l10n::$locales['one entry']                          = [
+    'une publication',
+    '%s publications',
+];
+l10n::$locales['no comments'] = 'aucun commentaire';
+l10n::$locales['one comment'] = [
+    'un commentaire',
+    '%s commentaires',
+];
 l10n::$locales['last day']                                                 = 'dernier jour';
 l10n::$locales['last week']                                                = 'dernière semaine';
 l10n::$locales['last month']                                               = 'dernier mois';
